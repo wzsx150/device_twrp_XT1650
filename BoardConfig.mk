9aa90ff687722578c10ecc2a943bd7d294868f9c
wzsx150 <wzsx150@163.com>
2016-09-17 19:22:04 +0800

    add smp and ion, try

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,8 +44,12 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_CPU_SMP := true
+TW_NEW_ION_HEAP := true
+TARGET_USES_ION := true
+TARGET_USES_MOTOROLA_LOG := true
 
-# Init
+# Init  --not use
 #TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
 # Kernel
